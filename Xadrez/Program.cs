9ea4baa6e2a5fdbe2f9bfeda12368118afa9808c
Fernanda Loretto <fernanda.loretto3105@gmail.com>
2021-12-07 13:26:02 -0300

    Classe Posicao-Tabuleiro criada

---
+++ Xadrez/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Tabuleiro;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + P);
+            Console.ReadLine();
+        }
+    }
+}
