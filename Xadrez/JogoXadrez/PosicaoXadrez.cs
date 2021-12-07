adfc6448e5c0cb7487cc86a3324370f6010322c5
Fernanda Loretto <fernanda.loretto3105@gmail.com>
2021-12-07 14:46:40 -0300

    Classe PosicaoXadrez -JogoXadrez criada

---
+++ Xadrez/JogoXadrez/PosicaoXadrez.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using tabuleiro;
+
+namespace jogoXadrez
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna.ToString().ToUpper() + Linha;
+        }
+    }
+}
--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -8,15 +8,10 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
-
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(2, 4));
-
-            Tela.ImprimirTabuleiro(tab);
-
+            PosicaoXadrez pos = new PosicaoXadrez('c', 1);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosicao());
             Console.ReadLine();
-        }
+        } 
     }
 }
