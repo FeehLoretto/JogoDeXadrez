3d766c27cc4cb4c0f0a12e50837d7df2bb88a007
Fernanda Loretto <fernanda.loretto3105@gmail.com>
2021-12-08 15:00:08 -0300

    Jogada especial Promoçao Para Dama

--- Xadrez/JogoXadrez/PartidaDeXadrez.cs
+++ Xadrez/JogoXadrez/PartidaDeXadrez.cs
@@ -140,6 +140,22 @@ namespace jogoXadrez
                 DesfazMovimento(origem, destino, capturada);
                 throw new TabuleiroException("Você não pode se colocar em Xeque!");
             }
+
+            Peca p = Tab.Peca(destino);
+
+            // #JogadaEspecial Pomoção para Dama
+            if (p is Peao)
+            {
+                if((p.Cor == Cor.Azul && destino.Linha == 0) || (p.Cor == Cor.Vermelha && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -156,8 +172,6 @@ namespace jogoXadrez
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = Tab.Peca(destino);
 
             // #JogadaEspecial En Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
